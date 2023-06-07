a1be3d649523e8e41ae403cca33e73bb07533bfc
AlexDPaz <alexsandrodp1@gmail.com>
2023-06-06 22:19:34 -0300

    GET list

---
+++ src/main/java/com/aulapratica/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.aulapratica.dslist.dto;
+
+import com.aulapratica.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId(); 
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
